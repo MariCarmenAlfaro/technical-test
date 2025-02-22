a04f07ded9dd936d1db699744f518b56703c031c
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2025-02-22 16:27:23 +0100

    add routes & coming soon pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SongsComponent } from './components/songs/songs.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { CompaniesComponent } from './components/companies/companies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "home", component: HomeComponent},
+    {path : "songs", component: SongsComponent},
+    {path : "artists", component: ArtistsComponent},
+    {path : "companies", component: CompaniesComponent}
+];
