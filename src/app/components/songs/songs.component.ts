69ce155bbf6a91c9ab2c11e012ff9f7a786d8d66
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2025-02-24 20:25:25 +0100

    songs component structure

--- src/app/components/songs/songs.component.ts
+++ src/app/components/songs/songs.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ComunicaionService } from '../../services/comunication.service';
+import { Subscription } from 'rxjs';
+import { MockService } from '../../services/mock.service';
+import { SongEntity } from '../../entities/songs/song.interface';
+import { ArtistEntity } from '../../entities/artists/artist.interface';
 
 @Component({
   selector: 'app-songs',
@@ -7,6 +12,33 @@ import { Component } from '@angular/core';
   templateUrl: './songs.component.html',
   styleUrl: './songs.component.scss'
 })
-export class SongsComponent {
+export class SongsComponent implements OnInit {
+  title=""
+  private subscription: Subscription = new Subscription;
+  songsList: SongEntity[] = []
+  artistsList: ArtistEntity[] = []
+
+  constructor(private comunicationService: ComunicaionService, private mockService: MockService){
+
+  }
+  ngOnInit(): void {
+    this.subscription = this.comunicationService.mensaje$.subscribe(m => {
+      this.title = m; 
+      console.log(this.title) 
+    });
+    this.mockService.getSongs().subscribe((rs)=>{
+      this.songsList = rs
+      console.log(this.songsList)
+    })
+    this.mockService.getArtists().subscribe((rs)=>{
+      this.artistsList = rs
+      console.log(this.artistsList)   
+        this.songsList.forEach(s => {
+        var artist = this.artistsList.find(x => Number(x.id) == s.artist); 
+        s.artistName = artist ? artist.name : '';
+      });
+    })
+ 
+  }
 
 }
---
+++ src/app/entities/songs/song.interface.ts
@@ -0,0 +1,11 @@
+export interface SongEntity{
+    id: string;                
+    title: string;             
+    artist: number; 
+    artistName: string;           
+    duration: number;            
+    genre: string[];            
+    year: number;             
+    poster: string;          
+    rating: number; 
+}
