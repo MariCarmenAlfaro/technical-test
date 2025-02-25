0d8efe5c4da0d535bd5db2764bc100b49e841a63
MariCarmenAlfaro <101742672+MariCarmenAlfaro@users.noreply.github.com>
2025-02-25 22:34:08 +0100

    first form, example styles, create song

--- src/app/components/songs/create-song/create-song.component.ts
+++ src/app/components/songs/create-song/create-song.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { SongsService } from '../../../services/songs.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-create-song',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, DatePickerModule],
   templateUrl: './create-song.component.html',
   styleUrl: './create-song.component.scss'
 })
 export class CreateSongComponent implements OnInit{
-  constructor(private songservice: SongsService){
+  songForm: FormGroup
+  @Input() countries: any[] =[]
+  @Input() artists: any[]=[]
+  @Input() genres: any[]=[]
+  @Input() companies: any[]=[]
+  @Output() songFormValues = new EventEmitter<any>(); 
 
-  }
+
+  constructor(private fb: FormBuilder ){ }
   ngOnInit(): void {
-
+    this.songForm = this.fb.group({
+      title: ['', Validators.required],
+      artist: ['', Validators.required],
+      genre: [[], Validators.required],
+      recordLabel: [[], Validators.required],
+      country: ['', Validators.required],
+      year: [null, [Validators.required]],
+      rating: [null, [Validators.required]]
+    });
   }
 
+  saveNewSong() {
+    this.songFormValues.emit(this.songForm.value)
+  }
 }
--- src/app/components/songs/songs.component.ts
+++ src/app/components/songs/songs.component.ts
@@ -1,44 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-import { ComunicaionService } from '../../services/comunication.service';
-import { Subscription } from 'rxjs';
-import { MockService } from '../../services/mock.service';
 import { SongEntity } from '../../entities/songs/song.interface';
 import { ArtistEntity } from '../../entities/artists/artist.interface';
+import { CompaniesEntity } from '../../entities/companies/companies.interface';
+import { SongsService } from '../../services/songs.service';
+import { ArtistsService } from '../../services/artists.service';
+import { CompaniesService } from '../../services/companies.service';
+import { CreateSongComponent } from './create-song/create-song.component';
+import { CountriesService } from '../../services/countries.service';
+import { GenresService } from '../../services/genres.service';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-songs',
   standalone: true,
-  imports: [],
+  imports: [CreateSongComponent,ToastModule],
   templateUrl: './songs.component.html',
-  styleUrl: './songs.component.scss'
+  styleUrl: './songs.component.scss',
 })
 export class SongsComponent implements OnInit {
-  title=""
-  private subscription: Subscription = new Subscription;
-  songsList: SongEntity[] = []
-  artistsList: ArtistEntity[] = []
+  songsList: SongEntity[] = [];
+  artistsList: ArtistEntity[] = [];
+  companiesList: CompaniesEntity[] = [];
+  countriesList: any[] = [];
+  genresList: any[] = [];
+  newSong: boolean = false;
 
-  constructor(private comunicationService: ComunicaionService, private mockService: MockService){
-
-  }
+  constructor(
+    private songsService: SongsService,
+    private artistsService: ArtistsService,
+    private companiesService: CompaniesService,
+    private countriesService: CountriesService,
+    private genresServices: GenresService,
+    private messageService: MessageService
+  ) {}
   ngOnInit(): void {
-    this.subscription = this.comunicationService.mensaje$.subscribe(m => {
-      this.title = m; 
-      console.log(this.title) 
+    this.readAllSongs();
+    this.countriesService.getAllCountries().subscribe((rs) => {
+      this.countriesList = rs;
     });
-    this.mockService.getSongs().subscribe((rs)=>{
-      this.songsList = rs
-      console.log(this.songsList)
-    })
-    this.mockService.getArtists().subscribe((rs)=>{
-      this.artistsList = rs
-      console.log(this.artistsList)   
-        this.songsList.forEach(s => {
-        var artist = this.artistsList.find(x => Number(x.id) == s.artist); 
+    this.companiesService.getAllCompanies().subscribe((rs) => {
+      this.companiesList = rs;
+    });
+    this.genresServices.getAllGenre().subscribe((rs) => {
+      this.genresList = rs;
+    });
+    this.artistsService.getAllArtists().subscribe((rs) => {
+      this.artistsList = rs;
+      this.songsList.forEach((s) => {
+        var artist = this.artistsList.find((x) => Number(x.id) == s.artist);
         s.artistName = artist ? artist.name : '';
       });
-    })
- 
+    });
   }
 
+  readAllSongs(){
+    this.songsService.getAllSongs().subscribe((rs) => {
+      this.songsList = rs;
+    });
+
+  }
+  createNewSong(rq) {
+    var request =     {
+      "title": rq.title,
+      "genre": rq.genre,
+      "poster": "http://dummyimage.com/400x600.png/dddddd/000000",
+      "duration":0,
+      "year": new Date(rq.year).getFullYear(),
+      "rating": rq.rating,
+      "artist": Number(rq.artist)
+    }
+    this.songsService.postSongs(request).subscribe((rs)=>{
+      if(rs != null){
+        this.messageService.add({ severity: 'success', detail: 'Canción guardada' });
+        this.newSong = false;
+        this.readAllSongs();
+      }
+    })
+  }
+
+  // editSong($event){
+
+  // }
 }
